d9dccceedf3ddfbc0b6dcb321d7ac3af541efda4
Leuthra <romidev20@gmail.com>
2026-04-22 19:57:41 +0700

    feat: add task with input

---
+++ src/basic/task/LuasKelilingPersegiPanjang.java
@@ -0,0 +1,27 @@
+package basic.task;
+
+import java.util.Scanner;
+
+/**
+ * Program untuk menghitung Luas dan Keliling Persegi Panjang.
+ * Input dibaca dari keyboard menggunakan kelas Scanner.
+ */
+public class LuasKelilingPersegiPanjang {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan panjang persegi = ");
+        int panjang = input.nextInt();
+
+        System.out.print("Masukan lebar persegi = ");
+        int lebar = input.nextInt();
+
+        int luas = panjang * lebar;
+        int keliling = 2 * (panjang + lebar);
+
+        System.out.println("Luas Persegi Panjang = " + luas);
+        System.out.println("Keliling Persegi Panjang = " + keliling);
+
+        input.close();
+    }
+}
