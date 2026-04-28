5f9e7bdc9b1c5515d2dc08b60c50e11a8fb1d2e8
leuthra <romidev20@gmail.com>
2026-04-28 22:21:03 +0700

    feat: add category age task

---
+++ src/basic/task/KategoriUsia.java
@@ -0,0 +1,28 @@
+package basic.task;
+
+import java.util.Scanner;
+
+/**
+ * Program untuk menentukan kategori usia.
+ * Input dibaca dari keyboard menggunakan kelas Scanner.
+ */
+public class KategoriUsia {
+    public static void main(String[] args) {
+        Scanner input = new Scanner(System.in);
+
+        System.out.print("Masukan Usia kamu = ");
+        int usia = input.nextInt();
+
+        if (usia >= 1 && usia <= 16) {
+            System.out.println("Kategori Usia Kamu adalah Anak-anak");
+        } else if (usia >= 17 && usia <= 25) {
+            System.out.println("Kategori Usia Kamu adalah Remaja");
+        } else if (usia > 25 && usia <=55) {
+            System.out.println("Kategori Usia Kamu adalah Dewasa");
+        } else {
+            System.out.println("Kategori Usia Kamu tidak valid (di luar rentang table)");
+        }
+
+        input.close();
+    }
+}
